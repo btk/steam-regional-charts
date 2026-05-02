a1ad10d1783f8c780be055b6a5bd3ba82090cd7e
Burak Tokak <iletisim@buraktokak.com>
2026-05-02 18:21:20 +0700

    googel anylsfsicns

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,43 @@
-import "@/styles/globals.css";
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Script from 'next/script';
+
+import '@/styles/globals.css';
+
+const GA_MEASUREMENT_ID = 'G-CJFHP6212L';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+
+  useEffect(() => {
+    const onRouteChange = (url) => {
+      if (typeof window.gtag !== 'function') return;
+      window.gtag('config', GA_MEASUREMENT_ID, {
+        page_path: url,
+      });
+    };
+
+    router.events.on('routeChangeComplete', onRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
